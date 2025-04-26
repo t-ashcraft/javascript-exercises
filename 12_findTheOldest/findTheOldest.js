8378922d7a54386de6e0f12f40397d38d3b3d0e2
t-ashcraft <termisdash@gmail.com>
2025-04-26 13:22:02 -0400

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
 const findTheOldest = function() {
-
+    let years = arguments[0].map((obj) => ((obj.yearOfDeath != undefined)?obj.yearOfDeath:2025) - obj.yearOfBirth)
+    let ind = 0;
+    let highest = 0;
+    for (let i = 0; i < years.length; i++) {
+        if (years[i] > highest) {
+            ind = i;
+            highest = years[i];
+        }
+    }
+    return arguments[0][ind];
 };
 
 // Do not edit below this line
