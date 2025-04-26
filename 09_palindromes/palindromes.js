ac6e92c484fb5cc05fd81876945a69bd39f02675
t-ashcraft <termisdash@gmail.com>
2025-04-26 13:04:27 -0400

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const isPunc = function(c) {
+    return (c !="!" && c!="." && c!="," && c!="?" && c!=" "); 
+}
 
+const palindromes = function () {
+    let arr = Array.from(arguments[0]);    
+    
+    arr = arr.filter(isPunc);
+    let string = arr.toString()
+    let str = string.toLowerCase();
+    
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
