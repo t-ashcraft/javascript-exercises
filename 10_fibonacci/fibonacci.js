26f2b0580e16b4f32e7ad5aa76bfff4b9fbed581
t-ashcraft <termisdash@gmail.com>
2025-04-26 13:10:05 -0400

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
 const fibonacci = function() {
-
+    const num = parseInt(arguments[0]);
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num == 0) {
+        return 0;
+    }
+    if (num == 1 || num == 2) {
+        return 1
+    }
+    return fibonacci(num-1) + fibonacci(num-2);
 };
 
 // Do not edit below this line
